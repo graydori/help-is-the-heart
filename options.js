dc2c2c2a05225974718ad1434eb91f313ad5f380
Shan <shan@naziripour.com>
2019-07-24 17:45:28 -0700

    More updates

--- options.js
+++ options.js
@@ -2,29 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+"use strict";
+const page = $("#feedback");
 
-const page = $('#feedback');
-
-function constructOptions(data) {
-  for (let item of data) {
+function constructOptions({feedback}) {
+  debugger;
+  for (let item of feedback) {
     let card = $(`<div class="card" style="width: 18rem;">
-      <img src="${data.img}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${data.title}</h5>
-        <p class="card-text">${data.text}</p>
-        <a href="#" class="btn btn-secondary">Discard</a>
-        <a href="#" class="btn btn-primary">Send Feedback</a>
-      </div>
+    <img src="${item.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${item.title}</h5>
+      <p class="card-text">${item.text}</p>
+      <a href="#" class="btn btn-secondary">Discard</a>
+      <a href="#" class="btn btn-primary">Send Feedback</a>
     </div>
-    `);
-    card.on('click', 'btn-primary', function() {
-      alert('Re submit');
-    }).on('click', 'btn-secondary', function() {
-      card.remove();
-    });
+  </div>
+  `);
+    card
+      .on("click", "btn-primary", function() {
+        alert("Re submit");
+      })
+      .on("click", "btn-secondary", function() {
+        card.remove();
+      });
     page.append(button);
   }
 }
 
-chrome.storage.sync.get('feedback', constructOptions );
+chrome.storage.sync.get("feedback", constructOptions);
--- popup.js
+++ popup.js
@@ -2,36 +2,69 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
-const feedback = $('#feedback');
-const search = $('#search').on('change', function() {
-  const term = search.attr('value');
-  $.get(`https://www.linkedin.com/help/api/hip/v1/search/linkedin/jobs?query=${term}`);
-  //Re prese
+"use strict";
+const list = $("#list");
+const search = $("#search").keyup(function() {
+  const term = search.val();
+  if (term) {
+    $.get(
+      `https://www.linkedin.com/help/api/hip/v1/search/linkedin/jobs?query=${term}`,
+      function(data) {
+        list.html(""); //clear the list
+        $.each(data.contentList, function(i, d) {
+          list.append(
+            `<a target="_blank" href="${domain}${
+              d.url
+            }" class="list-group-item list-group-item-action">${d.title}</a>`
+          );
+        });
+      }
+    );
+  }
 });
 
-const walkthrough = $('#walkthrough');
-const form = $('#form').submit(function() {
+const walkthrough = $("#walkthrough");
+const form = $("form").submit(function() {
+  debugger;
   return false;
 });
 let ff = [];
-chrome.storage.sync.get('feedback', function(data) {
-  ff = data;
+chrome.storage.sync.get("feedback", function({ feedback }) {
+  debugger;
+  ff = feedback;
 });
 
+const domain = "https://www.linkedin.com";
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  const url = tabs[0].url;
+  const path = url.substring(url.indexOf(domain) + domain.length);
+  $.getJSON(`${domain}/help/api/hip/v1/show/linkedin${path}`, function(data) {
+    list.html(""); //clear the list
+    $.each(data.contentList, function(i, d) {
+      list.append(
+        `<a target="_blank" href="${domain}${
+          d.url
+        }" class="list-group-item list-group-item-action">${d.title}</a>`
+      );
+    });
+  });
+});
 
-https://www.linkedin.com/help/api/hip/v1/show/linkedin/jobs
-
-feedback.click(function(element) {
-   ff.push({ title: 'Test', text: prompt("Describe your issues and ideas", search.attr('value')) });
-   chrome.storage.sync.set('feedback',feedback);
+const feedback = $("#feedback").click(function(element) {
+  ff.push({
+    title: "Test",
+    text: prompt("Describe your issues and ideas", search.val())
+  });
+  chrome.storage.sync.set({ feedback: ff }, function() {
+    console.log("saved");
+  });
 });
 
 walkthrough.click(function(element) {
-  let color = walkthrough.attr('value');
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "black";'});
+  let color = walkthrough.attr("value");
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: 'document.body.style.backgroundColor = "black";'
+    });
   });
-})
+});
